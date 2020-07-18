a7416b6bb20676b8a228146f87543f92274b4ae8
EasierByCode <Daniel@EasierByCode.com>
2020-07-18 13:42:15 -0700

    add ship trail

--- src/js/player.ts
+++ src/js/player.ts
@@ -3,14 +3,33 @@ export class Player extends Phaser.GameObjects.Sprite {
     
     constructor( scene, x, y ) {
         super( scene, x, y, 'ship-boy' );
-        
-        scene.add.existing( this );
         scene.physics.world.enableBody( this );
 
-        this.body.setCollideWorldBounds(true);
+        var particles = scene.add.particles( 'blue' );
 
-        // this.setScale( 0.015 );  // eva-cola
+        var emitter = particles.createEmitter({
+            speed: 125,
+            lifespan: {
+                onEmit: (particle, key, t, value) =>
+                {
+                    return Phaser.Math.Between( 200, 300 );
+                }
+            },
+            angle: {
+                onEmit: (particle, key, t, value) =>
+                {
+                    var v = Phaser.Math.Between(-15, 15);
+                    return (this.angle - 270) + v;
+                }
+            },
+            scale: { start: 0.275, end: 0 },
+            blendMode: 'ADD'
+        });
+
+        this.body.setCollideWorldBounds(true);
         this.setScale( 0.25 );
+        emitter.startFollow( this );
+        scene.add.existing( this );
     }
 
 }
